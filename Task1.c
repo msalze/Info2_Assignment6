3d992b14b8225127b82c8d04041afb83aaf30960
Melanie <melanie.salzer@uzh.ch>
2018-05-20 16:05:23 +0200

    add test cases. knapsackDynamic still with bug

--- Task1.c
+++ Task1.c
@@ -27,6 +27,7 @@ void print(int n, int capacity){
 }
 
 int knapsackRec(int capacity, int mass[], int profit[], int n){
+
 	if (capacity <= 0 ||n<0){return 0;}
 	if (mass[n]<=capacity){
 		int new_capacity = capacity-mass[n];
@@ -41,6 +42,7 @@ int knapsackRec(int capacity, int mass[], int profit[], int n){
 }
 
 int knapsackMemoized(int capacity, int mass[], int profit[], int n, int m[n][capacity]){
+	init();
 	if (m[n][capacity]>=0){
 		return (m[n][capacity]);
 	}
@@ -66,20 +68,22 @@ int knapsackMemoized(int capacity, int mass[], int profit[], int n, int m[n][cap
 }
 
 
-int knapsackDynamic(int capacity, int mass[], int profit[], int n, int m[n][capacity]){
+int knapsackDynamic(int capacity, int mass[], int profit[], int n){
+	init();
 	int i = 0;
 	int j = 0;
-	for (i=0; i<=n; i++){
-		j=0;
+	for (i=0; i<n; i++){
 		for (j=0; j<=capacity; j++){
-			
 			if (j == 0){
 				m[i][j]=0;
 			}
 			else if (mass[i]<=j){
 				int new_capacity = j - mass[i];
 				int take = profit[i] + m[i-1][new_capacity];
-				int leave = m[i-1][j];
+				int leave=0;
+				if (i>0){
+					leave = m[i-1][j];
+				}
 				if (take<leave){
 					m[i][j]=leave;
 				}
@@ -98,11 +102,12 @@ int knapsackDynamic(int capacity, int mass[], int profit[], int n, int m[n][capa
 }
 
 void main(){
+	/*
 	int mass[10] ={2,4,3,2};
 	int profit[10] = {45,40,25,15};
 	int n=4;
 	int capacity = 5;
-	/*
+	
 	int max_rec = knapsackRec(capacity, mass,profit,n);
 	printf("max_rec=%d\n",max_rec);
 	
@@ -110,10 +115,74 @@ void main(){
 	init();
 	int max_memo=knapsackMemoized(capacity,mass,profit,n,m);
 	printf("max_memo=%d\n",max_memo);
-	*/
+	
 	init();
 	print(4,5);
 	int max_dyn=knapsackDynamic(capacity,mass,profit,n,m);
 	printf("max_dyn=%d\n",max_dyn);
 	print(4,5);
+	*/
+	
+	time_t t;
+	srand((unsigned) time(&t));
+	//create the knapsacks
+	int mass1[4] = {6,3,2,4};
+	int profit1[4] = {50,60,40,20};
+	int n1 = 4;
+	int capacity1 = 10;
+	
+	int mass2[10] = {23,31,29,44,53,58,63,85,89,82};
+	int profit2[10] = {92,57,49,68,60,43,67,84,87,72};
+	int n2 = 10;
+	int capacity2 = 165;
+	
+	int mass3[50];
+	int i=0;
+	for(i = 0;i<50;i++){
+		mass3[i] = rand() % 100 + 1;
+	}
+	int profit3[50];
+	for(i = 0;i<50;i++){
+		profit3[i] = rand() % 100 + 1;
+	}
+	int n3 = 50;
+	int capacity3 = 500;
+	// A) Recursive
+	printf("Recursive Way:\n");
+	
+	int profitRecursive1 = knapsackRec(capacity1, mass1, profit1, n1-1);
+	printf("max profit 1: %d\n",profitRecursive1);
+	
+	int profitRecursive2 = knapsackRec(capacity2, mass2, profit2, n2-1);
+	printf("max profit 2: %d\n",profitRecursive2);
+	
+	// B) Memoized	
+	printf("Memoized Way:\n");
+	
+	init();
+	int profitMemoized1 = knapsackMemoized(capacity1,mass1,profit1,n1,m);
+	printf("max profit: %d\n",profitMemoized1);
+	
+	init();
+	int profitMemoized2 = knapsackMemoized(capacity2,mass2,profit2,n2,m);
+	printf("max profit: %d\n",profitMemoized2);
+	
+	init();
+	int profitMemoized3 = knapsackMemoized(capacity3,mass3,profit3,n3,m);
+	printf("max profit: %d\n",profitMemoized3);
+// C) Dynamic	
+	printf("Dynamic Way:\n");
+	
+	init();
+	int profitDynamic1 = knapsackDynamic(capacity1,mass1,profit1,n1);
+	printf("max profit: %d\n",profitDynamic1);
+	
+	init();
+	int profitDynamic2 = knapsackDynamic(capacity2,mass2,profit2,n2);
+	printf("max profit: %d\n",profitDynamic2);
+	
+	init();
+	int profitDynamic3 = knapsackDynamic(capacity3,mass3,profit3,n3);
+	printf("max profit: %d\n",profitDynamic3);
+	
 }
