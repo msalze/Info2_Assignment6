e12a4fc913322fc8efc7656641110ab524ee35e9
Melanie <melanie.salzer@uzh.ch>
2018-05-20 14:05:11 +0200

    beginning of sth great

--- Task2.c
+++ Task2.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int m[50][50]
+int m[50][50][50];
 
-int trade(int n, int k, int A[]){
+int trade(int n, int k, int price[]){
 	int i = 0;
-	int j = =;
+	int j = 0;
+	int status = 0; //0 if not bought, 1 if has object
 	for (i;i<n;i++){
-		for
+		for (j=0;j<k){
+			if (i==0 || j==0){
+				m[i][j]=0;
+			}
+			if (j>2*i-1){
+				m[i][j]=m[i][j-1];
+			}
+			else{
+				int trade=0;
+				int dont=m[i][j-1];
+				if (status == 0){
+					trade = 
+				}
+			}
+			
+		}
 	}
+	return m[n][k];
 }
 
 
